26e3cc37210b15079223a487033d495e603d4371
Sohee Kim <wneh2023@naver.com>
2025-11-22 21:25:50 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cout << "|\\_/|\n";
+	cout << "|q p|   /}\n";
+	cout << "( 0 )\"\"\"\\\n";
+	cout << "|\"^\"`    |\n";
+	cout << "||_/=\\\\__|\n";
+}
