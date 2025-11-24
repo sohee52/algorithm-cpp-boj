98cfa48cecdff7d2de99985730f274858f08a450
Sohee Kim <wneh2023@naver.com>
2025-11-24 16:28:08 +0900

    [Silver II] Title: 통계학, Time: 116 ms, Memory: 4376 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -10,29 +10,38 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
         sum += a[i];
-        v.push_back(a[i]);
-        if (mp.find(a[i]) != mp.end()) mp[a[i]]++;
-        else mp.insert({a[i], 1});
+        mp[a[i]]++;
         mx = max(a[i], mx);
         mn = min(a[i], mn);
     }
 
-    double k = (double) sum / n;
-    cout << floor(k + 0.5) << "\n";
+    cout << (int) round((double)sum / n) << "\n";
 
-    sort(v.begin(), v.end());
-    cout << v[n/2] << "\n";
 
-    int many = -1;
+    sort(a, a + n);
+    cout << a[n/2] << "\n";
+
+    // 방법 1
+    int many = 0;
     for (auto it : mp) {
         many = max(many, it.second);
     }
     for (auto it : mp) {
         if (it.second == many) num.push_back(it.first);
     }
-    sort(num.begin(), num.end());
-    if (num.size() > 1) cout << num[1] << "\n";
-    else cout << num[0] << "\n";
 
+    // 방법 2
+    // map<int, vector<int>> freq_map; // {빈도 -> 해당 빈도를 가진 값들}
+    // int many = 0;
+
+    // for (auto it : mp) {
+    //     freq_map[it.second].push_back(it.first);
+    //     many = max(many, it.second);
+    // }
+
+    // num = freq_map[many]; // 최대 빈도를 가진 값들
+
+    sort(num.begin(), num.end());
+    cout << (num.size() > 1 ? num[1] : num[0]) << "\n";
     cout << mx - mn << "\n";
 }
