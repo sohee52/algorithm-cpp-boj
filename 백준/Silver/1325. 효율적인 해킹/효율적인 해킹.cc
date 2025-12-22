e82543494eb402269e48804e5250cc8117154baf
Sohee Kim <wneh2023@naver.com>
2025-12-22 23:55:25 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2560 ms, Memory: 3540 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,30 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a, b, visited[10004], dp[10004], mx;
-vector<int> v[10004];
-int dfs(int here) {
-	visited[here] = 1;
-	int ret = 1;
-	for (int there : v[here]) {
-		if (visited[there]) continue;
-		ret += dfs(there);
-	}
-	return ret;
+const int MAX = 10004;
+int visited[MAX], dp[MAX];
+vector<int> v[MAX];
+
+int go(int x) {
+    int cnt = 1;
+    visited[x] = 1;
+    for (int nx : v[x]) {
+        if (visited[nx]) continue;
+        cnt += go(nx);
+    }
+    return cnt;
 }
+
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m;
-	while (m--) {
-		cin >> a >> b;
-		v[b].push_back(a);
-	}
-	for (int i = 1; i <= n; i++) {
-		memset(visited, 0, sizeof(visited));
-		dp[i] = dfs(i);
-		mx = max(mx, dp[i]);
-	}
-	for (int i = 1; i <= n; i++) {
-		if (mx == dp[i]) cout << i << " ";
-	}
+	cin.tie(nullptr);
+	int n, m;
+    cin >> n >> m;
+
+    int a, b;
+    while(m--) {
+        cin >> a >> b;
+        v[b].push_back(a);
+    }
+
+    int mx = -1;
+    for (int i = 1; i <= n; i++) {
+        memset(visited, 0, sizeof(visited));
+        dp[i] = go(i);
+        mx = max(dp[i], mx);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (mx == dp[i]) cout << i << " ";
+    }
 }
